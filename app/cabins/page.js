3c069f11f7283c2bd4894d4722f09d0bd254b9fa
Mikiyas6 <Mikitedi9@gmail.com>
2025-04-24 21:16:15 +0300

    Fetching and Displaying Cabin List

--- app/cabins/page.js
+++ app/cabins/page.js
@@ -1,13 +1,14 @@
 import CabinCard from "@/app/_components/CabinCard";
+import { getCabin, getCabins } from "../_lib/data-service";
 
 export const metadata = {
   title: "Cabins",
   description: "A place to relax and unwind",
 };
 
-export default function Page() {
+export default async function Page() {
   // CHANGE
-  const cabins = [];
+  const cabins = await getCabins();
 
   return (
     <div>
